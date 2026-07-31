93e97d8ca4105eab6f6d10f1c28c66e9e22d2f9d
kauafreireaureliano-dot <kauafreireaureliano@gmail.com>
2026-07-31 19:15:04 -0300

    Atualiza links de checkout para Cartpanda (Premium e Básico)

--- components/upgrade-popup.tsx
+++ components/upgrade-popup.tsx
@@ -47,7 +47,7 @@ export function UpgradePopup({
         currency: 'BRL'
       });
     }
-    window.location.href = 'https://ggcheckout.app/checkout/v2/YZdjit7Wh1dMX8FOAbHR'
+    window.location.href = 'https://jcmultii.mycartpanda.com/checkout/172571449:1'
   }
 
   const handleUpgradeDecline = () => {
@@ -58,7 +58,7 @@ export function UpgradePopup({
         currency: 'BRL'
       });
     }
-    window.location.href = 'https://ggcheckout.app/checkout/v2/O4PSYnUIBTKXuBQ3Odnn'
+    window.location.href = 'https://jcmultii.mycartpanda.com/checkout/172571568:1'
   }
 
   if (!show) return null
